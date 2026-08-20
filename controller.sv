5c5864e9ae3c38a586a4df4da8f4ba8f5522bf62
meghana reddy <baddipudim829@gmail.com>
2026-08-20 16:51:29 -0700

    Implement controller module with state machine

---
+++ controller.sv
@@ -0,0 +1,47 @@
+module controller(
+  input logic clk,
+  input logic rst,
+  input logic start,
+  input logic count_done,
+  output logic done,
+  output logic mac_en
+);
+  typedef enum logic [1:0] {IDLE, COMPUTE, DONE} state_t;
+  state_t state, next_state;
+
+  always_ff @(posedge clk) begin
+    if(rst) begin
+      state <= IDLE;
+    end
+    else begin
+      state <= next_state;
+    end
+  end
+
+  always_comb begin
+    case(state)
+      IDLE:
+        if(start) begin
+          next_state = COMPUTE;
+        end
+        else begin
+          next_state = IDLE;
+        end
+      COMPUTE:
+        if(count_done) begin
+          next_state = DONE;
+        end
+        else begin
+          next_state = IDLE;
+        end
+      DONE: next_state = IDLE;
+
+      default: next_state = IDLE;
+    endcase
+  end
+
+  assign done = DONE;
+  assign mac_en = COMPUTE;
+  
+endmodule
+      
