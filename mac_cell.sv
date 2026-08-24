a1cb7cd24c414f4c986de46248d7d425ce375e51
meghana reddy <baddipudim829@gmail.com>
2026-08-24 15:35:16 -0700

    Update mac_cell inputs and outputs to 32 and 64 bits

--- mac_cell.sv
+++ mac_cell.sv
@@ -2,11 +2,11 @@ module mac_cell (
   input logic clk,
   input logic rst,
   input logic mac_en,
-  input logic w_in,
-  input logic x_in,
-  output logic x_out,
-  output logic w_out,
-  output logic y
+  input logic [31:0] w_in,
+  input logic [31:0] x_in,
+  output logic [31:0] x_out,
+  output logic [31:0] w_out,
+  output logic [63:0] y
 );
 
   always_ff @(posedge clk) begin
@@ -18,7 +18,7 @@ module mac_cell (
     else if (mac_en) begin
       y <= y + (w_in * x_in);
       x_out <= x_in;
-      w_out <= x_out;
+      w_out <= w_in;
     end
   end
   
